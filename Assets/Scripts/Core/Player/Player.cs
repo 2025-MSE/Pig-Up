b43a28dee381231bc7a9f49fdc20d761df43713a
puter2712git <puter2712goo@gmail.com>
2025-04-20 00:59:21 +0900

    feat: Apply player model and animator

--- Assets/Scripts/Core/Player/Player.cs
+++ Assets/Scripts/Core/Player/Player.cs
@@ -13,12 +13,19 @@ namespace MSE.Core
         [SerializeField]
         private Camera m_Camera;
 
+        private Animator m_Animator;
+        public Animator Animator => m_Animator;
+
+        private void Awake()
+        {
+            m_Animator = GetComponentInChildren<Animator>();
+        }
+
         public override void OnNetworkSpawn()
         {
             if (!IsOwner) return;
 
             m_Camera.gameObject.SetActive(true);
-            Debug.Log($"IsOwner: {IsOwner}");
         }
     }
 
