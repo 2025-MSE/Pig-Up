f12a2b5c385ecf7eee1338004431e3f8eaf85504
Dongjin Kuk <puter2712goo@gmail.com>
2025-04-27 03:37:30 +0900

    feat: Transparent wall in map

--- Assets/Scripts/Core/Block/Block.cs
+++ Assets/Scripts/Core/Block/Block.cs
@@ -9,19 +9,34 @@ namespace MSE.Core
 
         private BlockBoundary m_Boundary;
 
+        private Renderer m_Renderer;
+
         private void Awake()
         {
             m_Boundary = GetComponentInChildren<BlockBoundary>();
+            m_Renderer = GetComponentInChildren<Renderer>();
         }
 
         public void ReadyToBuild()
         {
             m_Boundary.SetBoundaryActive(false);
+            foreach (var mat in m_Renderer.materials)
+            {
+                Color color = mat.color;
+                color.a = 0.5f;
+                mat.color = color;
+            }
         }
 
         public void OnBuilt()
         {
             m_Boundary.SetBoundaryActive(true);
+            foreach (var mat in m_Renderer.materials)
+            {
+                Color color = mat.color;
+                color.a = 1f;
+                mat.color = color;
+            }
         }
     }
 }
