23e392d532966922153fb0428c32b6ae8e4abaad
Dongjin Kuk <puter2712goo@gmail.com>
2025-05-27 00:45:33 +0900

    Add player name info

--- Assets/Scripts/Core/Player/Player.cs
+++ Assets/Scripts/Core/Player/Player.cs
@@ -2,13 +2,20 @@
  * Owner: Dongjin Kuk
  */
 
+using System.Collections.Generic;
 using Unity.Netcode;
-using Unity.Netcode.Components;
+using Unity.Services.Authentication;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace MSE.Core
 {
+    public struct PlayerTag
+    {
+        public string Id;
+        public string Name;
+        public Transform Transform;
+    }
+
     public class Player : NetworkBehaviour
     {
         [SerializeField]
@@ -16,6 +23,8 @@ namespace MSE.Core
 
         private Animator m_Animator;
         public Animator Animator => m_Animator;
+
+        private static List<PlayerTag> m_PlayerTags = new List<PlayerTag>();
 
         private void Awake()
         {
@@ -26,7 +35,45 @@ namespace MSE.Core
         public override void OnNetworkSpawn()
         {
             m_Camera.gameObject.SetActive(IsOwner);
+
+            // Spawn it's player name info.
+            if (!IsOwner)
+            {
+                SpawnNameInfoRpc(AuthenticationService.Instance.PlayerId, AuthenticationService.Instance.PlayerName);
+            }
+            else
+            {
+                foreach (var tag in m_PlayerTags)
+                {
+                    UICharacterInfoHandler.OnCharacterActivated?.Invoke(tag.Id, tag.Transform, tag.Name, transform);
+                }
+            }
+        }
+
+        public override void OnNetworkDespawn()
+        {
+            if (!IsOwner)
+            {
+                RemoveNameInfoRpc(AuthenticationService.Instance.PlayerId);
+            }
+        }
+
+        [Rpc(SendTo.ClientsAndHost)]
+        private void SpawnNameInfoRpc(string id, string name)
+        {
+            if (NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject() == null)
+            {
+                m_PlayerTags.Add(new PlayerTag { Id = id, Name = string.IsNullOrEmpty(name) ? id : name, Transform = transform });
+                return;
+            }
+
+            UICharacterInfoHandler.OnCharacterActivated?.Invoke(id, transform, string.IsNullOrEmpty(name) ? id : name, NetworkManager.Singleton.SpawnManager.GetLocalPlayerObject().transform);
+        }
+
+        [Rpc(SendTo.ClientsAndHost)]
+        private void RemoveNameInfoRpc(string id)
+        {
+            UICharacterInfoHandler.OnCharacterDeactivated?.Invoke(id);
         }
     }
-
 }
